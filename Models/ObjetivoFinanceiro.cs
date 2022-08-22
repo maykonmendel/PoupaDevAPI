4001008c1bad0b8a07c206a7c16abda3a864e5d5
Maykon de Castro Mendel <maykonmendel.dev@gmail.com>
2022-08-22 09:46:59 -0300

    Removing ? nullable for atributes

--- Models/ObjetivoFinanceiro.cs
+++ Models/ObjetivoFinanceiro.cs
@@ -8,13 +8,13 @@ namespace PoupaDevAPI.Models
     public class ObjetivoFinanceiro : IEntity<int>
     {
         public int Id { get; set; }
-        public string? Titulo { get; set; }
-        public string? Descricao { get; set; }
-        public decimal? ValorObjetivo { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public decimal ValorObjetivo { get; set; }
         public DateTime DataCriacao { get; set; }
         public List<OperacaoFinanceira> ListaOperacoes { get; set; }
 
-        public ObjetivoFinanceiro(string? titulo, string? descricao, decimal? valorObjetivo)
+        public ObjetivoFinanceiro(string titulo, string descricao, decimal valorObjetivo)
         {            
             Titulo = titulo;
             Descricao = descricao;
